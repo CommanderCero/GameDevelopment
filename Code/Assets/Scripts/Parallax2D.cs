15a4827d74eed48a78931a4792a53cda56899f5e
Sarah <me@sarah.games>
2022-11-14 12:45:42 +0000

    Parallax is working in test Scene

--- Code/Assets/Scripts/Parallax2D.cs
+++ Code/Assets/Scripts/Parallax2D.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Parallax2D : MonoBehaviour
+
 {
+    private float length, startpos;
+    public float parallaxFactor;
+    public GameObject cam;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = cam.transform.position.x * parallaxFactor;
+        Vector3 newPosition = new Vector3(startpos + distance, transform.position.y, transform.position.z);
+        transform.position = newPosition;
+
     }
 }
